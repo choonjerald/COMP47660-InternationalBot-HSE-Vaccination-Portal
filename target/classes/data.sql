c38d01b74ec219077705075df6d65299d4a6ec1d
shijieliu789 <shijie.liu@ucdconnect.ie>
2022-05-16 16:04:31 +0100

    target files to allow merge

---
+++ target/classes/data.sql
@@ -0,0 +1,38 @@
+INSERT INTO vaccination_centres(id, name)
+VALUES (1, "Ongar Community Centre");
+INSERT INTO vaccination_centres(id, name)
+VALUES (2, "University College Dublin");
+INSERT INTO vaccination_centres(id, name)
+VALUES (3, "RDS Vaccination Centre");
+INSERT INTO appointments(id, appointment_type, date, is_Booked, time, user, vaccination_centre)
+VALUES (1, 'First Dose', '2022-03-12', FALSE, '13:10:00', NULL, 1);
+INSERT INTO appointments(id, appointment_type, date, is_Booked, time, user, vaccination_centre)
+VALUES (2, 'First Dose', '2022-03-12', FALSE, '13:40:00', NULL, 1);
+INSERT INTO appointments(id, appointment_type, date, is_Booked, time, user, vaccination_centre)
+VALUES (3, 'First Dose', '2022-03-13', FALSE, '13:10:00', NULL, 1);
+INSERT INTO appointments(id, appointment_type, date, is_Booked, time, user, vaccination_centre)
+VALUES (4, 'First Dose', '2022-03-13', FALSE, '13:40:00', NULL, 1);
+INSERT INTO appointments(id, appointment_type, date, is_Booked, time, user, vaccination_centre)
+VALUES (5, 'First Dose', '2022-03-14', FALSE, '13:10:00', NULL, 1);
+INSERT INTO appointments(id, appointment_type, date, is_Booked, time, user, vaccination_centre)
+VALUES (6, 'First Dose', '2022-03-12', FALSE, '12:10:00', NULL, 2);
+INSERT INTO appointments(id, appointment_type, date, is_Booked, time, user, vaccination_centre)
+VALUES (7, 'First Dose', '2022-03-12', FALSE, '12:40:00', NULL, 2);
+INSERT INTO appointments(id, appointment_type, date, is_Booked, time, user, vaccination_centre)
+VALUES (8, 'First Dose', '2022-03-13', FALSE, '12:10:00', NULL, 2);
+INSERT INTO appointments(id, appointment_type, date, is_Booked, time, user, vaccination_centre)
+VALUES (9, 'First Dose', '2022-03-12', FALSE, '13:10:00', NULL, 3);
+INSERT INTO appointments(id, appointment_type, date, is_Booked, time, user, vaccination_centre)
+VALUES (10, 'First Dose', '2022-03-12', FALSE, '13:40:00', NULL, 3);
+INSERT INTO appointments(id, appointment_type, date, is_Booked, time, user, vaccination_centre)
+VALUES (11, 'First Dose', '2022-03-12', FALSE, '14:10:00', NULL, 3);
+INSERT INTO appointments(id, appointment_type, date, is_Booked, time, user, vaccination_centre)
+VALUES (12, 'First Dose', '2022-03-13', FALSE, '18:40:00', NULL, 3);
+INSERT INTO appointments(id, appointment_type, date, is_Booked, time, user, vaccination_centre)
+VALUES (13, 'First Dose', '2022-03-15', FALSE, '13:00:00', NULL, 3);
+INSERT INTO appointments(id, appointment_type, date, is_Booked, time, user, vaccination_centre)
+VALUES (14, 'First Dose', '2022-03-15', FALSE, '13:30:00', NULL, 3);
+INSERT INTO appointments(id, appointment_type, date, is_Booked, time, user, vaccination_centre)
+VALUES (15, 'First Dose', '2022-03-15', FALSE, '14:00:00', NULL, 3);
+INSERT INTO appointments(id, appointment_type, date, is_Booked, time, user, vaccination_centre)
+VALUES (16, 'First Dose', '2022-03-15', FALSE, '14:30:00', NULL, 3);
